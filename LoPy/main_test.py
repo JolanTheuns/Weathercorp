a924b3efc445b80cca5e81baf5c11c3f40665b84
JolanTheuns <jolantheuns@gmail.com>
2020-01-14 09:19:09 +0100

    deleted unused files

--- LoPy/main_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from main import loraSetup
-import socket
-
-class testMain(unittest.TestCase):
-    def test_LoraSetup(self):
-        print(self.assertIsInstance(loraSetup(),socket))
